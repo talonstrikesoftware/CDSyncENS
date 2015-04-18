a7a4a31b444f8a9f18b323e5fc25718f5b498064
Mark Astin <redwolf89@gmail.com>
2015-04-18 08:55:45 -0400

    Initial Commit

---
+++ CDSyncENS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CDSyncENS
+//
+//  Created by Mark Astin on 4/18/15.
+//  Copyright (c) 2015 Talon Strike Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
